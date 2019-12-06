e0122a29816cd0351b4030445997725d565409d9
Stephen Lai <100024043@mvla.net>
2019-12-05 21:50:59 -0800

    files updated

--- Runner.java
+++ Runner.java
@@ -2,15 +2,12 @@ import javax.swing.JFrame;
  
  
 public class Runner {
-   
     public static void main(String[] args) {
- 
  
         JFrame frame = new JFrame("Circle");
  
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  
-        //Create panel and add it to the frame
         Screen sc = new Screen();
          
         frame.add(sc);
@@ -18,5 +15,6 @@ public class Runner {
         frame.setVisible(true);
          
         sc.animate();
+        
     }
 }
